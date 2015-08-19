99424b106a1079550518d8aea08f6f376098b038
Kevin Stensberg <kstensberg@users.noreply.github.com>
2015-08-19 00:53:43 -0700

    Unicorn Sparkles now shows up in the live wallpaper selection list

---
+++ app/src/main/java/stensberg/kevin/unicornsparkles/MainActivity.java
@@ -0,0 +1,22 @@
+package stensberg.kevin.unicornsparkles;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setWallpaper(View v) {
+        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, new ComponentName(this, UnicornSparklesWallpaperService.class));
+        startActivity(intent);
+    }
+}
